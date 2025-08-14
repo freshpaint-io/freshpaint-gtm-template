3851f0547a10d7321ca5411ef3fab0c159287afb
Tiffany Han <tiffany@freshpaint.io>
2025-08-13 19:28:47 -0700

    poc for breaking up params

---
+++ src/parameters/tags/root.ts
@@ -0,0 +1,105 @@
+[
+  {
+    "type": "SELECT",
+    "name": "tagType",
+    "displayName": "Freshpaint Tag Type",
+    "macrosInSelect": false,
+    "selectItems": [
+      {
+        "value": "ga4Event",
+        "displayValue": "Google Analytics 4 (Proxy)"
+      },
+      {
+        "value": "googleAdsEvent",
+        "displayValue": "Google Ads"
+      },
+      {
+        "value": "googleAdsCallConversionsEvent",
+        "displayValue": "Google Ads Call Conversions"
+      },
+      {
+        "value": "googleCM360Event",
+        "displayValue": "Google Campaign Manager 360 Conversions API"
+      },
+      {
+        "value": "fbPixelEvent",
+        "displayValue": "Facebook Conversions API"
+      },
+      {
+        "value": "basisEvent",
+        "displayValue": "Basis"
+      },
+      {
+        "value": "viantEvent",
+        "displayValue": "Viant"
+      },
+      {
+        "value": "linkedInAdsCAPIEvent",
+        "displayValue": "LinkedIn Ads Conversions API"
+      },
+      {
+        "value": "mntnEvent",
+        "displayValue": "MNTN"
+      },
+      {
+        "value": "bingAdsEvent",
+        "displayValue": "Bing Ads"
+      },
+      {
+        "value": "impactEvent",
+        "displayValue": "impact.com"
+      },
+      {
+        "value": "stackAdaptEvent",
+        "displayValue": "StackAdapt"
+      },
+      {
+        "value": "theTradeDeskEvent",
+        "displayValue": "theTradeDesk"
+      },
+      {
+        "value": "tikTokAdsEvent",
+        "displayValue": "TikTok Ads"
+      },
+      {
+        "value": "pinterestAdsEvent",
+        "displayValue": "Pinterest Ads"
+      },
+      {
+        "value": "redditAdsEvent",
+        "displayValue": "Reddit Ads"
+      },
+      {
+        "value": "twitterAdsEvent",
+        "displayValue": "Twitter Ads"
+      },
+      {
+        "value": "addEventProperties",
+        "displayValue": "AddEventProperties"
+      },
+      {
+        "value": "identify",
+        "displayValue": "Identify"
+      },
+      {
+        "value": "track",
+        "displayValue": "Track"
+      },
+      {
+        "value": "linkedInAdsEvent",
+        "displayValue": "LinkedIn Ads"
+      },
+      {
+        "value": "floodlightEvent",
+        "displayValue": "Floodlight - DEPRECATED - instead use Google Campaign Manager 360 Conversions API"
+      }
+    ],
+    "notSetText": "-",
+    "simpleValueType": true,
+    "valueValidators": [
+      {
+        "type": "NON_EMPTY"
+      }
+    ]
+  }
+]
