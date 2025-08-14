cec99bfc764298a3d5d8f07d9e0d84dbe697fc74
Tiffany Han <tiffany@freshpaint.io>
2025-08-14 14:28:57 -0700

    add tdd

---
+++ src/parameters/tags/integrations/tdd.ts
@@ -0,0 +1,45 @@
+import { commonEventName, equals, nonEmpty, text } from '../helpers';
+
+export default function TddParams() {
+  const isTddEvent = equals('tagType', 'theTradeDeskEvent');
+  const onlyForTdd = [isTddEvent];
+
+  return [
+    text({
+      name: 'theTradeDeskInstanceName',
+      displayName: 'Specific Advertiser ID (optional)',
+      help: 'If multiple Advertiser IDs are configured for theTradeDesk destination type, specify one to deliver to (if left blank, this event will be delivered to all configured Advertiser IDs)',
+      simpleValueType: true,
+      enablingConditions: onlyForTdd,
+    }),
+    commonEventName(onlyForTdd),
+    text({
+      name: 'theTradeDeskTrackerOrUPixelIDValue',
+      displayName: 'Event Tracker ID',
+      help: '(a.k.a. Tracking Tag ID as used in the Static Tracking Tag)',
+      simpleValueType: true,
+      enablingConditions: onlyForTdd,
+      valueValidators: [nonEmpty()],
+    }),
+    text({
+      name: 'theTradeDeskValue',
+      displayName: 'Value',
+      simpleValueType: true,
+      enablingConditions: onlyForTdd,
+    }),
+    text({
+      name: 'theTradeDeskPrivacySettings',
+      displayName: 'Privacy Settings array (optional)',
+      help: 'Privacy Settings array object',
+      simpleValueType: true,
+      enablingConditions: onlyForTdd,
+    }),
+    text({
+      name: 'theTradeDeskDataProcessingOption',
+      displayName: 'Data Processing Option (optional)',
+      help: 'Data Processing Option object',
+      simpleValueType: true,
+      enablingConditions: onlyForTdd,
+    }),
+  ];
+}
