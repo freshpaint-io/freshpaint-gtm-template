5896cb55dc77c9ecd5390d254f4ba94f2d574b33
hwolff00 <hwolff00@gmail.com>
2025-10-20 19:48:30 -0400

    common selectable event.

--- src/parameters/common.ts
+++ src/parameters/common.ts
@@ -1,6 +1,7 @@
 import {
   EnablingCondition,
   Text,
+  Select,
   text,
   nonEmpty,
   simpleTable,
@@ -17,6 +18,72 @@ export const commonEventName = (enablingConditions: Array<EnablingCondition> = [
     help: 'This will be the event name that you see in Freshpaint - it is sent to only certain destinations, including Google Analytics 4 (Proxy).',
     simpleValueType: true,
     valueValidators: [nonEmpty()],
+    enablingConditions,
+  });
+};
+
+export const commonSelectableEventName = (
+  enablingConditions: Array<EnablingCondition> = [],
+): Select => {
+  const allEventNames = [
+    // Snapchat events
+    // See: https://developers.snap.com/api/marketing-api/Conversions-API/Parameters#server-parameters
+    { value: 'ACHIEVEMENT_UNLOCKED', displayValue: 'Snapchat Achievement Unlocked' },
+    { value: 'ADD_BILLING', displayValue: 'Snapchat Add Billing' },
+    { value: 'ADD_CART', displayValue: 'Snapchat Add Cart' },
+    { value: 'ADD_TO_WISHLIST', displayValue: 'Snapchat Add To Wishlist' },
+    { value: 'AD_CLICK', displayValue: 'Snapchat Ad Click' },
+    { value: 'AD_VIEW', displayValue: 'Snapchat Ad View' },
+    { value: 'APP_INSTALL', displayValue: 'Snapchat App Install' },
+    { value: 'APP_OPEN', displayValue: 'Snapchat App Open' },
+    { value: 'COMPLETE_TUTORIAL', displayValue: 'Snapchat Complete Tutorial' },
+    { value: 'CUSTOM_EVENT_1', displayValue: 'Snapchat Custom Event 1' },
+    { value: 'CUSTOM_EVENT_2', displayValue: 'Snapchat Custom Event 2' },
+    { value: 'CUSTOM_EVENT_3', displayValue: 'Snapchat Custom Event 3' },
+    { value: 'CUSTOM_EVENT_4', displayValue: 'Snapchat Custom Event 4' },
+    { value: 'CUSTOM_EVENT_5', displayValue: 'Snapchat Custom Event 5' },
+    { value: 'INVITE', displayValue: 'Snapchat Invite' },
+    { value: 'LEVEL_COMPLETE', displayValue: 'Snapchat Level Complete' },
+    { value: 'LIST_VIEW', displayValue: 'Snapchat List View' },
+    { value: 'LOGIN', displayValue: 'Snapchat Login' },
+    { value: 'PAGE_VIEW', displayValue: 'Snapchat Page View' },
+    { value: 'PURCHASE', displayValue: 'Snapchat Purchase' },
+    { value: 'RATE', displayValue: 'Snapchat Rate' },
+    { value: 'RESERVE', displayValue: 'Snapchat Reserve' },
+    { value: 'SAVE', displayValue: 'Snapchat Save' },
+    { value: 'SEARCH', displayValue: 'Snapchat Search' },
+    { value: 'SHARE', displayValue: 'Snapchat Share' },
+    { value: 'SIGN_UP', displayValue: 'Snapchat Sign Up' },
+    { value: 'SPENT_CREDITS', displayValue: 'Snapchat Spent Credits' },
+    { value: 'START_CHECKOUT', displayValue: 'Snapchat Start Checkout' },
+    { value: 'START_TRIAL', displayValue: 'Snapchat Start Trial' },
+    { value: 'SUBSCRIBE', displayValue: 'Snapchat Subscribe' },
+    { value: 'VIEW_CONTENT', displayValue: 'Snapchat View Content' },
+
+    // Spotify events
+    // See: https://adshelp.spotify.com/s/article/Spotify-Conversions-API-US
+    // Note: the doc above is not clear on names, but each event below has been manually verified by calling the Spotify CAPI with that event_name.
+    { value: 'ALIAS', displayValue: 'Spotify CAPI Alias' },
+    { value: 'ADD_TO_CART', displayValue: 'Spotify CAPI Add to Cart' },
+    { value: 'CUSTOM_EVENT_1', displayValue: 'Spotify CAPI Custom Event 1' },
+    { value: 'CUSTOM_EVENT_2', displayValue: 'Spotify CAPI Custom Event 2' },
+    { value: 'CUSTOM_EVENT_3', displayValue: 'Spotify CAPI Custom Event 3' },
+    { value: 'CUSTOM_EVENT_4', displayValue: 'Spotify CAPI Custom Event 4' },
+    { value: 'CUSTOM_EVENT_5', displayValue: 'Spotify CAPI Custom Event 5' },
+    { value: 'LEAD', displayValue: 'Spotify CAPI Lead' },
+    { value: 'VIEW', displayValue: 'Spotify CAPI Page View' },
+    { value: 'PURCHASE', displayValue: 'Spotify CAPI Purchase' },
+    { value: 'SIGN_UP', displayValue: 'Spotify CAPI Sign Up' },
+    { value: 'CHECK_OUT', displayValue: 'Spotify CAPI Start Checkout' },
+    { value: 'PRODUCT', displayValue: 'Spotify CAPI View Product' },
+  ];
+  return select({
+    name: 'commonSelectableEventName',
+    displayName: 'Freshpaint Selectable Event Name',
+    help: 'Scroll for destination specific event names below.',
+    simpleValueType: true,
+    selectItems: allEventNames,
+    defaultValue: 'PURCHASE',
     enablingConditions,
   });
 };
