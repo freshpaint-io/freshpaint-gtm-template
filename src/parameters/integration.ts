62e027ae8de4a4cd98fb3e22cddbd535da4fe59c
Kyle Wright <kylejamwright@gmail.com>
2025-11-03 15:48:29 -0700

    moved sort-refactor to separate PR

--- src/parameters/integration.ts
+++ src/parameters/integration.ts
@@ -1,25 +1,25 @@
-export const basisEvent = 'basisEvent';
-export const bingAdsEvent = 'bingAdsEvent';
-export const fbPixelEvent = 'fbPixelEvent';
 export const ga4Event = 'ga4Event';
+export const googleAdsEvent = 'googleAdsEvent';
 export const googleAdsCallConversionsEvent = 'googleAdsCallConversionsEvent';
 export const googleAdsConversionApiEvent = 'googleAdsConversionApiEvent';
-export const googleAdsEvent = 'googleAdsEvent';
 export const googleCM360Event = 'googleCM360Event';
-export const impactEvent = 'impactEvent';
+export const fbPixelEvent = 'fbPixelEvent';
+export const basisEvent = 'basisEvent';
+export const viantEvent = 'viantEvent';
 export const linkedInAdsCAPIEvent = 'linkedInAdsCAPIEvent';
 export const mntnEvent = 'mntnEvent';
-export const nextdoorEvent = 'nextdoorEvent';
-export const pinterestAdsEvent = 'pinterestAdsEvent';
-export const redditAdsEvent = 'redditAdsEvent';
-export const siriusXMEvent = 'siriusXMEvent';
+export const bingAdsEvent = 'bingAdsEvent';
+export const impactEvent = 'impactEvent';
 export const snapchatEvent = 'snapchatEvent';
-export const spotifyCAPIEvent = 'spotifyCAPIEvent';
 export const stackAdaptEvent = 'stackAdaptEvent';
 export const theTradeDeskEvent = 'theTradeDeskEvent';
 export const tikTokAdsEvent = 'tikTokAdsEvent';
+export const pinterestAdsEvent = 'pinterestAdsEvent';
+export const redditAdsEvent = 'redditAdsEvent';
+export const siriusXMEvent = 'siriusXMEvent';
+export const spotifyCAPIEvent = 'spotifyCAPIEvent';
 export const twitterAdsEvent = 'twitterAdsEvent';
-export const viantEvent = 'viantEvent';
+export const nextdoorEvent = 'nextdoorEvent';
 export const yelpCAPIEvent = 'yelpCAPIEvent';
 export const addEventProperties = 'addEventProperties';
 export const identify = 'identify';
@@ -28,28 +28,28 @@ export const linkedInAdsEvent = 'linkedInAdsEvent';
 export const floodlightEvent = 'floodlightEvent';
 
 export const rootParamSelectItems = [
-  { value: basisEvent, displayValue: 'Basis' },
-  { value: bingAdsEvent, displayValue: 'Bing Ads' },
-  { value: fbPixelEvent, displayValue: 'Facebook Conversions API' },
   { value: ga4Event, displayValue: 'Google Analytics 4 (Proxy)' },
+  { value: googleAdsEvent, displayValue: 'Google Ads' },
   { value: googleAdsCallConversionsEvent, displayValue: 'Google Ads Call Conversions' },
   { value: googleAdsConversionApiEvent, displayValue: 'Google Ads Conversion API' },
-  { value: googleAdsEvent, displayValue: 'Google Ads' },
   { value: googleCM360Event, displayValue: 'Google Campaign Manager 360 Conversions API' },
-  { value: impactEvent, displayValue: 'impact.com' },
+  { value: fbPixelEvent, displayValue: 'Facebook Conversions API' },
+  { value: basisEvent, displayValue: 'Basis' },
+  { value: viantEvent, displayValue: 'Viant' },
   { value: linkedInAdsCAPIEvent, displayValue: 'LinkedIn Ads Conversions API' },
   { value: mntnEvent, displayValue: 'MNTN' },
-  { value: nextdoorEvent, displayValue: 'Nextdoor' },
-  { value: pinterestAdsEvent, displayValue: 'Pinterest Ads' },
-  { value: redditAdsEvent, displayValue: 'Reddit Ads' },
-  { value: siriusXMEvent, displayValue: 'SiriusXM' },
+  { value: bingAdsEvent, displayValue: 'Bing Ads' },
+  { value: impactEvent, displayValue: 'impact.com' },
   { value: snapchatEvent, displayValue: 'Snapchat' },
-  { value: spotifyCAPIEvent, displayValue: 'Spotify Conversions API' },
   { value: stackAdaptEvent, displayValue: 'StackAdapt' },
   { value: theTradeDeskEvent, displayValue: 'theTradeDesk' },
   { value: tikTokAdsEvent, displayValue: 'TikTok Ads' },
+  { value: pinterestAdsEvent, displayValue: 'Pinterest Ads' },
+  { value: redditAdsEvent, displayValue: 'Reddit Ads' },
+  { value: siriusXMEvent, displayValue: 'SiriusXM' },
+  { value: spotifyCAPIEvent, displayValue: 'Spotify Conversions API' },
   { value: twitterAdsEvent, displayValue: 'Twitter Ads' },
-  { value: viantEvent, displayValue: 'Viant' },
+  { value: nextdoorEvent, displayValue: 'Nextdoor' },
   { value: yelpCAPIEvent, displayValue: 'Yelp Conversions API' },
   { value: addEventProperties, displayValue: 'AddEventProperties' },
   { value: identify, displayValue: 'Identify' },
@@ -66,32 +66,32 @@ export const rootParamSelectItems = [
 ];
 
 export const trackDestinationSelectItems = [
-  { value: 'Amplitude', displayValue: 'Amplitude' },
-  { value: 'Basis', displayValue: 'Basis' },
-  { value: 'Bing Ads', displayValue: 'Bing Ads' },
-  { value: 'Facebook Conversions API', displayValue: 'Facebook Conversions API' },
-  { value: 'Google Ads Conversion API', displayValue: 'Google Ads Conversion API' },
-  { value: 'Google AdWords New', displayValue: 'Google Ads' },
   { value: 'Google Analytics 4 Proxy', displayValue: 'Google Analytics 4 (Proxy)' },
   { value: 'Google Analytics 4', displayValue: 'Google Analytics 4 (Server-Side)' },
+  { value: 'Google AdWords New', displayValue: 'Google Ads' },
+  { value: 'Google Ads Conversion API', displayValue: 'Google Ads Conversion API' },
   {
     value: 'Google Campaign Manager 360 Conversions API',
     displayValue: 'Google Campaign Manager 360 Conversions API',
   },
+  { value: 'Facebook Conversions API', displayValue: 'Facebook Conversions API' },
+  { value: 'Amplitude', displayValue: 'Amplitude' },
+  { value: 'Basis', displayValue: 'Basis' },
+  { value: 'Bing Ads', displayValue: 'Bing Ads' },
   { value: 'impactdotcom', displayValue: 'impact.com' },
   { value: 'LinkedIn Ads Conversions API', displayValue: 'LinkedIn Ads Conversions API' },
-  { value: 'Mixpanel', displayValue: 'Mixpanel' },
   { value: 'MNTN', displayValue: 'MNTN' },
-  { value: 'Nextdoor', displayValue: 'Nextdoor' },
-  { value: 'pinterest-ads', displayValue: 'Pinterest Ads' },
-  { value: 'reddit-ads', displayValue: 'Reddit Ads' },
-  { value: 'SiriusXM', displayValue: 'SiriusXM' },
+  { value: 'Mixpanel', displayValue: 'Mixpanel' },
   { value: 'Snapchat', displayValue: 'Snapchat' },
   { value: 'Spotify Conversions API', displayValue: 'Spotify Conversions API' },
   { value: 'StackAdapt', displayValue: 'StackAdapt' },
   { value: 'theTradeDesk', displayValue: 'theTradeDesk' },
   { value: 'TikTok Ads', displayValue: 'TikTok Ads' },
+  { value: 'reddit-ads', displayValue: 'Reddit Ads' },
+  { value: 'pinterest-ads', displayValue: 'Pinterest Ads' },
+  { value: 'SiriusXM', displayValue: 'SiriusXM' },
   { value: 'Twitter Ads', displayValue: 'Twitter Ads' },
+  { value: 'Nextdoor', displayValue: 'Nextdoor' },
   { value: 'viant', displayValue: 'Viant' },
   { value: 'Yelp Conversions API', displayValue: 'Yelp Conversions API' },
   { value: 'Webhooks', displayValue: 'Webhooks' },
